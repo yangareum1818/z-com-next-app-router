9a813c9b050015f614619bfa35f71bf7c99817d0
areumYang <yangareum1818@nate.com>
2024-01-17 01:44:33 +0900

    feat: 게시글 다중 이미지 조건부 css처리

---
+++ src/app/(afterLogin)/_component/PostImages.tsx
@@ -0,0 +1,132 @@
+import Link from "next/link";
+import style from "./post.module.css";
+import cx from "classnames";
+
+type Props = {
+  post: {
+    postId: number;
+    User: {
+      id: string;
+      image: string;
+      nickname: string;
+    };
+    content: string;
+    createAt: Date;
+    Images: any[];
+  };
+};
+
+export default function PostImages({ post }: Props) {
+  if (!post.Images) return null; // 이미지가 없을 경우
+  if (!post.Images.length) return null; // 배열이 아닐 경우 또는 배열의 갯수가 없을 때
+  if (post.Images.length === 1) {
+    return (
+      <div className={cx(style.postImageSection, style.oneImage)}>
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[0].imageId}`}
+          className={style.postImageSectionLink}
+          style={{
+            backgroundImage: `url(${post.Images[0]?.link})`,
+            backgroundSize: "cover",
+          }}
+        >
+          <img src={post.Images[0]?.link} alt="" />
+        </Link>
+      </div>
+    );
+  }
+  if (post.Images.length === 2) {
+    return (
+      <div className={cx(style.postImageSection, style.twoImage)}>
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[0].imageId}`}
+          className={style.postImageSectionLink}
+          style={{
+            backgroundImage: `url(${post.Images[0]?.link})`,
+            backgroundSize: "cover",
+          }}
+        ></Link>
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[1].imageId}`}
+          className={style.postImageSectionLink}
+          style={{
+            backgroundImage: `url(${post.Images[1]?.link})`,
+            backgroundSize: "cover",
+          }}
+        ></Link>
+      </div>
+    );
+  }
+  if (post.Images.length === 3) {
+    return (
+      <div className={cx(style.postImageSection, style.threeImage)}>
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[0].imageId}`}
+          className={style.postImageSectionLink}
+          style={{
+            backgroundImage: `url(${post.Images[0]?.link})`,
+            backgroundSize: "cover",
+          }}
+        ></Link>
+        <div>
+          <Link
+            href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[1].imageId}`}
+            className={style.postImageSectionLink}
+            style={{
+              backgroundImage: `url(${post.Images[1]?.link})`,
+              backgroundSize: "cover",
+            }}
+          ></Link>
+          <Link
+            href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[2].imageId}`}
+            className={style.postImageSectionLink}
+            style={{
+              backgroundImage: `url(${post.Images[2]?.link})`,
+              backgroundSize: "cover",
+            }}
+          ></Link>
+        </div>
+      </div>
+    );
+  }
+  if (post.Images.length === 4) {
+    return (
+      <div className={cx(style.postImageSection, style.fourImage)}>
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[0].imageId}`}
+          className={style.postImageSectionLink}
+          style={{
+            backgroundImage: `url(${post.Images[0]?.link})`,
+            backgroundSize: "cover",
+          }}
+        ></Link>
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[1].imageId}`}
+          className={style.postImageSectionLink}
+          style={{
+            backgroundImage: `url(${post.Images[1]?.link})`,
+            backgroundSize: "cover",
+          }}
+        ></Link>
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[2].imageId}`}
+          className={style.postImageSectionLink}
+          style={{
+            backgroundImage: `url(${post.Images[2]?.link})`,
+            backgroundSize: "cover",
+          }}
+        ></Link>
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[3].imageId}`}
+          className={style.postImageSectionLink}
+          style={{
+            backgroundImage: `url(${post.Images[3]?.link})`,
+            backgroundSize: "cover",
+          }}
+        ></Link>
+      </div>
+    );
+  }
+
+  return null;
+}
