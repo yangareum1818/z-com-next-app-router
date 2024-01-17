5310b05ee30b84060044bc24649eb2824eb532bb
areumYang <yangareum1818@nate.com>
2024-01-18 00:31:00 +0900

    feat : 레이아웃 반응형

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const path = require("path");
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   // pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
 };
 
 module.exports = nextConfig;
