9cd40b69d34381e67d5f5d499a54a7e88009969b
areumYang <yangareum1818@nate.com>
2024-01-24 16:38:44 +0900

    feat : next-auth setting env환경변수 설정

--- src/auth.ts
+++ src/auth.ts
@@ -6,5 +6,28 @@ export const {
   auth,
   signIn,
 } = NextAuth({
-  providers: [CredentialsProvider({})],
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        const authResponse = await fetch(
+          `${process.env.AUTH_URL}/users/login`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(credentials),
+          }
+        );
+
+        if (!authResponse.ok) {
+          return null;
+        }
+
+        const user = await authResponse.json();
+
+        return user;
+      },
+    }),
+  ],
 });
