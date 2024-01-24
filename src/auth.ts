7c7ed38a76ca98cf763ed62b4fc391fd8de830dc
areumYang <yangareum1818@nate.com>
2024-01-24 16:14:15 +0900

    feat : next-auth setting
    middleware, catch-all라우트, API라우트 만들기,

---
+++ src/auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+} = NextAuth({
+  providers: [CredentialsProvider({})],
+});
