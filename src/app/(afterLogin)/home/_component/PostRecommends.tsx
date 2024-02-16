fd246a7271eb7376fdec231486b6ba4db0e5ff30
areumYang <yangareum1818@nate.com>
2024-02-16 20:48:29 +0900

    feat : react-query
    gcTime, staleTime 설정

--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -9,6 +9,8 @@ export default function PostRecommends() {
   const { data } = useQuery<IPost[]>({
     queryKey: ["posts", "recommends"],
     queryFn: getPostRecommends,
+    staleTime: 60 * 1000, // fresh -> stale, 5분 기준
+    gcTime: 300 * 1000,
   });
 
   return (
