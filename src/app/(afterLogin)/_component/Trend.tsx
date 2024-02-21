a98562ab782d66e6e49fe7eea1db2b077943c130
areumYang <yangareum1818@nate.com>
2024-02-21 18:56:14 +0900

    commit

--- src/app/(afterLogin)/_component/Trend.tsx
+++ src/app/(afterLogin)/_component/Trend.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
 import style from "./trend.module.css";
+import { Hashtag } from "@/model/Hashtag";
 
-export default function Trend() {
+type Props = { trend: Hashtag };
+
+export default function Trend({ trend }: Props) {
   return (
-    <Link href={`/search?q=트렌드`} className={style.container}>
+    <Link href={`/search?q=${trend.title}`} className={style.container}>
       <div className={style.count}>실시간 트렌드</div>
-      <div className={style.title}>알음쨩</div>
-      <div className={style.count}>1,234 posts</div>
+      <div className={style.title}>{trend.title}</div>
+      <div className={style.count}>{trend.count.toLocaleString()}posts</div>
     </Link>
   );
 }
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,5 @@
 import { faker } from "@faker-js/faker";
-import { http, HttpResponse } from "msw";
+import { http, HttpResponse, StrictResponse } from "msw";
 
 function generateDate() {
   const lastWeek = new Date(Date.now());
@@ -54,7 +54,7 @@ export const handlers = [
     });
   }),
   http.get("/api/postRecommends", async () => {
-    console.log("posts 게시글 데이터");
+    console.log("추천 게시글 : posts 게시글 데이터");
 
     return HttpResponse.json([
       {
@@ -89,7 +89,7 @@ export const handlers = [
     ]);
   }),
   http.get("/api/followingPosts", async () => {
-    console.log("followingPosts 게시글 데이터");
+    console.log(" 팔로잉 중 : followingPosts 게시글 데이터");
 
     return HttpResponse.json([
       {
@@ -124,6 +124,7 @@ export const handlers = [
     ]);
   }),
   http.get("/api/search/:tag", async ({ request, params }) => {
+    // 검색 결과의 게시글
     const { tag } = params;
 
     return HttpResponse.json([
@@ -156,6 +157,138 @@ export const handlers = [
         Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
         createdAt: generateDate(),
       },
+    ]);
+  }),
+  http.get("/api/users/:userId/posts", ({ request, params }) => {
+    // 특정 사용자의 게시글
+    const { userId } = params;
+    return HttpResponse.json([
+      {
+        postId: 1,
+        User: User[0],
+        content: `${1} ${userId}의 게시글`,
+        Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+        createdAt: generateDate(),
+      },
+      {
+        postId: 2,
+        User: User[0],
+        content: `${2} ${userId}의 게시글`,
+        Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+        createdAt: generateDate(),
+      },
+      {
+        postId: 3,
+        User: User[0],
+        content: `${3} ${userId}의 게시글`,
+        Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+        createdAt: generateDate(),
+      },
+      {
+        postId: 4,
+        User: User[0],
+        content: `${4} ${userId}의 게시글`,
+        Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+        createdAt: generateDate(),
+      },
+      {
+        postId: 5,
+        User: User[0],
+        content: `${5} ${userId}의 게시글`,
+        Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+        createdAt: generateDate(),
+      },
+    ]);
+  }),
+  http.get("/api/users/:userId", ({ request, params }): StrictResponse<any> => {
+    // 특정 유저 정보
+    const { userId } = params;
+    const found = User.find((v) => v.id === userId);
+    if (found) {
+      return HttpResponse.json(found);
+    }
+    return HttpResponse.json(
+      { message: "no_such_user" },
+      {
+        status: 404,
+      }
+    );
+  }),
+  http.get("/api/posts/:postId", ({ request, params }): StrictResponse<any> => {
+    // 특정 사용자의 게시글 하나
+    const { postId } = params;
+    return HttpResponse.json({
+      postId,
+      User: User[0],
+      content: `${1} 게시글 아이디 ${postId}의 내용`,
+      Images: [
+        { imageId: 1, link: faker.image.urlLoremFlickr() },
+        { imageId: 2, link: faker.image.urlLoremFlickr() },
+        { imageId: 3, link: faker.image.urlLoremFlickr() },
+      ],
+      createdAt: generateDate(),
+    });
+  }),
+  http.get("/api/postId/:postId/comments", ({ request, params }) => {
+    // 게시글의 댓글
+    const { postId } = params;
+    return HttpResponse.json([
+      {
+        postId: 1,
+        User: User[0],
+        content: `${1} 게시글 ${postId}의 답글`,
+        Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+        createdAt: generateDate(),
+      },
+      {
+        postId: 2,
+        User: User[0],
+        content: `${2} 게시글 ${postId}의 답글`,
+        Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+        createdAt: generateDate(),
+      },
+      {
+        postId: 3,
+        User: User[0],
+        content: `${3} 게시글 ${postId}의 답글`,
+        Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+        createdAt: generateDate(),
+      },
+      {
+        postId: 4,
+        User: User[0],
+        content: `${4} 게시글 ${postId}의 답글`,
+        Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+        createdAt: generateDate(),
+      },
+      {
+        postId: 5,
+        User: User[0],
+        content: `${5} 게시글 ${postId}의 답글`,
+        Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+        createdAt: generateDate(),
+      },
+    ]);
+  }),
+  http.get("/api/followRecommends", ({ request }) => {
+    // 팔로우 추천
+    return HttpResponse.json(User);
+  }),
+  http.get("/api/trends", ({ request }) => {
+    // 나를 위한 트렌드
+    return HttpResponse.json([
+      { tagId: 1, title: "아롱이1", count: 152 },
+      { tagId: 2, title: "아롱이2", count: 112 },
+      { tagId: 3, title: "아롱이3", count: 12 },
+      { tagId: 4, title: "아롱이4", count: 872 },
+      { tagId: 5, title: "아롱이5", count: 42 },
+      { tagId: 6, title: "아롱이6", count: 51 },
+      { tagId: 7, title: "아롱이7", count: 92 },
+      { tagId: 8, title: "아롱이8", count: 652 },
+      { tagId: 9, title: "아롱이9", count: 94 },
+      { tagId: 10, title: "아롱이10", count: 42 },
+      { tagId: 11, title: "아롱이11", count: 78 },
+      { tagId: 12, title: "아롱이12", count: 96 },
     ]);
   }),
 ];
